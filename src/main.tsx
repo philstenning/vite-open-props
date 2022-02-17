0887b7dc7660bafc257d43015e79872325fa5413
Phil Stenning <philstenning@hotmail.com>
2022-02-17 09:43:44 +0000

    chore(Glob css for components): Glob css for components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app/App'
-import "open-props/style";
+// import "open-props/style";
 import './styles/index.css'
 ReactDOM.render(
   <React.StrictMode>
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Home() {
-  return <div>homr</div>;
+  return (<div>
+    
+    <button className="btn">hello</button>
+    <input type="checkbox" name="ckh" id="" />
+    <kbd>ctrl</kbd> + <kbd>alt</kbd> + <kbd>del</kbd>
+    home</div>);
 }
